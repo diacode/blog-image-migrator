9144b561832f94e0a07e31ecac4ada59e51147ba
Víctor Viruete <victor@diacode.com>
2015-03-23 20:31:17 +0100

    S3 Upload

--- script.rb
+++ script.rb
@@ -9,6 +9,7 @@ require 'aws-sdk'
 Dotenv.load
 
 POSTS_PATH = "/vagrant_data/diacode-website/source/blog"
+bucket = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
 
 Dir.glob("#{POSTS_PATH}/*.markdown") do |md_file_path|
   md_file_content = File.read(md_file_path)
@@ -25,11 +26,6 @@ Dir.glob("#{POSTS_PATH}/*.markdown") do |md_file_path|
 
   if matches.size > 0
     puts "Se han encontrado #{matches.size} imagenes en #{File.basename(md_file_path)}"
-    folder_name = File.basename(md_file_path, ".html.markdown")
-    folder_path = "#{POSTS_PATH}/#{folder_name}"
-    
-    puts "Creando carpeta #{folder_path}"
-    Dir.mkdir(folder_path) unless Dir.exist?(folder_path)
     
     matches.each do |match|
       image_url = match
@@ -38,13 +34,23 @@ Dir.glob("#{POSTS_PATH}/*.markdown") do |md_file_path|
 
       uri = URI.parse(image_url)
       image_name = File.basename(uri.path)
-
-      open("#{folder_path}/#{image_name}", 'wb') do |file|
+      tmp_image_path = "/tmp/#{image_name}"
+      
+      open(tmp_image_path, 'wb') do |file|
         file << open(image_url).read
       end
 
+      # Using same year/month structure as wordpress
+      date_matches = image_url.scan(/blog\.diacode\.com\/wp\-content\/uploads\/([0-9]{4})\/([0-9]{2})\/.*/i)
+      year = date_matches[0][0]
+      month = date_matches[0][1]
+
+      puts "Subiendo imagen #{image_name} a AWS S3..."
+      object = bucket.object("#{year}/#{month}/#{image_name}")
+      object.upload_file(tmp_image_path, acl: 'public-read')
+
       puts "Reemplazando ocurrencias..."
-      md_file_content = md_file_content.sub(image_url, "#{folder_name}/#{image_name}")
+      md_file_content = md_file_content.sub(image_url, object.public_url)
     end
 
     File.open(md_file_path, 'w') do |file|
